d6d70d692f791d0e48ab02fc2959e4a7317a4290
kota2neko <tm2tsk54@yahoo.co.jp>
2017-05-15 10:33:22 +0000

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 'use strict';
+const memo = new Map(); // メモ化による高速化
+memo.set(0, 0);
+memo.set(1, 1);
+
 function fib(n) {
-    if (n === 0) {
-        return 0;
-    } else if (n === 1) {
-        return 1;
+    if(memo.has(n)) {
+        return memo.get(n);
     }
-    return fib(n - 1) + fib(n - 2);
+
+    const value = fib(n - 1) + fib(n - 2);
+    memo.set(n, value);
+    return value;
 }
 const length = 40; // フィボナッチ数列を40番目まで出力する
 for (let i = 0; i <= length; i++) {
